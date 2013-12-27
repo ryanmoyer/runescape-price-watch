cf1fb6dfc55e785b53d9c8841fadc53dadc2916b
ryanmoyer <rymoyer@gmail.com>
2013-12-26 22:29:09 -0500

    Add a menu bar, status bar, and option to exit.

--- runescape_price_watch/gui.py
+++ runescape_price_watch/gui.py
@@ -9,4 +9,20 @@ class MainFrame(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, title=metadata.project, size=(200,100))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # Create a bar at bottom of window for status
 
+        # Add menu.
+        app_menu = wx.Menu()
+
+        # Add menubar.
+        menubar = wx.MenuBar()
+        menubar.Append(app_menu, '&App')
+        self.SetMenuBar(menubar)
+
+        # Add the option to exit.
+        menu_exit = app_menu.Append(wx.ID_EXIT, 'E&xit', ' Terminate the program')
+
+        self.Bind(wx.EVT_MENU, self._on_exit, menu_exit)
+
+    def _on_exit(self, event):
+        self.Close()
